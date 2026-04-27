d8ee313a004271bcc086ae6342e84799d78001e5
John <baabalola@gmail.com>
2026-04-27 17:15:03 +0100

    fix initial attached to s3 bucket

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 variable "bucket_suffix" {
   description = "Unique suffix for bucket name (use your initials)"
   type        = string
-  default     = "JB"  # CHANGE THIS to your initials!
+  default     = "john"  # CHANGE THIS to your initials!
 }
 
 # S3 Bucket - using fixed name to prevent duplicates
